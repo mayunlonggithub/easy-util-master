3dc97527e7ecb3b5d7a5c136b86b63d438aeabab
zxc2012 <786698925@qq.com>
2021-06-28 23:57:30 +0800

    update

--- easy-util-python/src/main/java/zju/edu/als/util/Executors.java
+++ easy-util-python/src/main/java/zju/edu/als/util/Executors.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package zju.edu.als.util;
 
 /**
@@ -25,31 +24,3 @@ public enum Executors {
         }
     }
 }
-=======
-package zju.edu.als.util;
-
-/**
- * @author Ma
- * @create 2019-09-13 20:08
- */
-public enum Executors {
-    PYTHON2,
-    PYTHON,//python or python3 depends on your platform
-    GENERAL;
-
-    private Executors() {
-    }
-
-    public String getId() {
-        return this.name().toLowerCase();
-    }
-
-    public static Executors fromId(String id) {
-        try {
-            return valueOf(id.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            return null;
-        }
-    }
-}
->>>>>>> 87707c62d24559d3cbe3dcc6995506cfbe9018e6
